36ca70d93c2cdfb583713ae2d1047e567568bd4e
hettling <hannes.hettling@gmail.com>
2016-02-22 15:30:42 +0100

    updated

--- src/workflow.sh
+++ src/workflow.sh
@@ -9,17 +9,7 @@ smrt-utils dbinsert -a ITSscorrigido.phy -p FERN -g -d "Fernanda, ITS data" -f p
 smrt-utils dbinsert -a PSBAcorrigido.phy -p FERN -g -d "Fernanda, PSBA data" -f phylip
 smrt-utils dbinsert -a trnlFcorrigido.phy  -p FERN -g -d "Fernanda, trnlF data" -f phylip
 
-# now, files with names ETScorrigido-smrt-inserted.fa have been created.
-# We still have to prepend the seed gi to the file name in order for
-# 'smrt orthologize to work' 
-for i in $(ls *smrt-inserted*); do a=`head -1 $i | cut -d '|' -f 2`; mv $i "$a-$i"; done
-
-# run supersmart pipeline steps for data retrieval
-smrt taxize -r Senecioneae -b
-smrt align
-
-# append inserted alignments to alignmen list
-ls *smrt-inserted*.fa >> aligned.txt
+# now, files with names *smrt-inserted*.fa >> aligned.txt
 
 # orthologize and make supermatrix for all taxa
 smrt orthologize
